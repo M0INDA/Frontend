681be307ca8f32c130d18ccf676f5a8197e15609
soyoung <0001401@naver.com>
2023-01-08 02:04:24 +0900

    [add] react-hook-form test

--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isDirty, errors },
+  } = useForm();
+
+  return (
+    <div className="w-[640px] bg-slate-500">
+      <form
+        className="border-spacing-1"
+        onSubmit={handleSubmit(async (data) => {
+          await new Promise((r) => setTimeout(r, 1000));
+          alert(JSON.stringify(data));
+        })}
+      >
+        <input
+          className="mr-5"
+          id="id"
+          type="text"
+          {...register("id", {
+            required: "이메일은 필수 입력입니다.",
+          })}
+        />
+        <input
+          className="mr-5"
+          id="password"
+          type="password"
+          {...register("password")}
+        />
+        <input
+          id="passwordcheck"
+          type="password"
+          {...register("passwordcheck")}
+        />
+        <input type="file" />
+        <button type="submit" disabled={isSubmitting}>
+          가입하기
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
