c53300f56716fee8b7cd9959cebc4e2cad587517
hansejun <tpwnszja2@gmail.com>
2023-01-14 03:38:51 +0900

    [add] axios와 cookie 코드 생성

--- src/apis/cookie.ts
+++ src/apis/cookie.ts
@@ -1,1 +1,18 @@
-export {};
+import { Cookies } from "react-cookie";
+
+const cookies = new Cookies();
+
+export const setAccessToken = (accessToken: string) => {
+  return cookies.set("Authorization", accessToken, {
+    sameSite: "strict",
+    path: "/",
+  });
+};
+
+export const getCookieToken = () => {
+  return cookies.get("Authorization");
+};
+
+export const removeCookieToken = () => {
+  return cookies.remove("Authorization", { sameSite: "strict", path: "/" });
+};
