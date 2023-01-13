f165c0400b346bcae8ee31ece675fa5627c8c63e
hansejun <tpwnszja2@gmail.com>
2023-01-14 04:20:11 +0900

    [add] 간단한 로그인 기능 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,45 @@
-import SignUp from "@components/signUp/SignUp";
+import { emailValid, passwordValid } from "@utils/valids";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+
+interface ILogin {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILogin>();
+
+  // 로그인 기능
+  const onValidSubmit = useCallback((data: ILogin) => {
+    //const response = await loginFn(data);
+    // 실패했을 때 함수
+    // 성공했을 때 함수
+  }, []);
+
+  // 카카오 로그인 버튼
+  const onClickKakao = useCallback(() => {}, []);
+
   return (
-    <>
-      <SignUp />
-    </>
+    <form
+      onSubmit={handleSubmit(onValidSubmit)}
+      className="mx-auto flex w-80 flex-col"
+    >
+      <input
+        {...register("email", emailValid())}
+        type="email"
+        placeholder="email"
+      />
+      <span>{errors.email?.message}</span>
+      <input {...register("password", passwordValid())} type="password" />
+      <span>{errors.password?.message}</span>
+      <button>로그인</button>
+      <button onClick={onClickKakao}>카카오 로그인</button>
+    </form>
   );
 };
 
