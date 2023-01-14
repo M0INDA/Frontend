8ecc45d91d2c8ebe26cb2207520264cef31d3d23
hansejun <tpwnszja2@gmail.com>
2023-01-15 01:33:16 +0900

    [add] useUser Hook 생성
    - 토큰이 존재하지 않거나 만료되었을 때 로그인 페이지 이동
    
    - 토큰을 디코딩하여 user 정보 리턴

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,38 @@
+import { removeCookieToken } from "@shared/cookie";
+import { Cookies } from "react-cookie";
+import { useEffect, useState } from "react";
+import { isExpired } from "react-jwt";
+import { useNavigate } from "react-router-dom";
+
+/**
+ * 토큰이 없거나 만료되었다면 login 페이지로 이동
+ * 토큰이 있다면 decoding한 정보를 리턴
+ */
+const useUser = () => {
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const cookies = new Cookies();
+    const myToken = cookies.get("Authorization");
+
+    if (!myToken) {
+      alert("로그인이 필요한 서비스입니다.");
+      return navigate("/login");
+    }
+
+    const isTokenExpired = isExpired(myToken);
+
+    if (isTokenExpired) {
+      alert("로그인이 필요한 서비스입니다.");
+      removeCookieToken();
+      return navigate("/login");
+    }
+
+    setUser(myToken);
+  }, [navigate]);
+
+  return user;
+};
+
+export default useUser;
