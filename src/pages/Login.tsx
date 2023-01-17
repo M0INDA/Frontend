9ac267f3464f2c9c2fa5fac96864be2d1f72bb70
hansejun <tpwnszja2@gmail.com>
2023-01-17 14:52:46 +0900

    #15 [add] 로그인 스타일 기본틀

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,9 @@ import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { ILogin } from "allTypes/user";
 import Input from "@elements/Input";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -21,25 +23,62 @@ const Login = () => {
   const onClickKakao = useCallback(() => {}, []);
 
   return (
-    <form
-      onSubmit={handleSubmit(onValidSubmit)}
-      className="mx-auto flex w-80 flex-col"
-    >
-      <Input
-        register={{ ...register("email", emailValid()) }}
-        type="email"
-        placeholder="Email"
-      />
-      <span>{errors.email?.message}</span>
-      <Input
-        register={{ ...register("password", passwordValid()) }}
-        type="password"
-        placeholder="Password"
-      />
-      <span>{errors.password?.message}</span>
-      <button>로그인</button>
-      <button onClick={onClickKakao}>카카오 로그인</button>
-    </form>
+    <div className="flex h-screen items-center justify-center bg-[#F6F6F6]">
+      <main className="flex h-full max-h-[812px] w-full max-w-[412px] flex-col bg-primary-100 p-[22px]">
+        <div className="relative flex items-center">
+          <span>&larr;</span>
+          <h1 className="absolute w-full text-center text-primary-main">
+            MOINDA
+          </h1>
+        </div>
+        <p className="text-center">
+          같은 목표를 가진 <br />
+          스터디원들과 모인다!
+        </p>
+        <span className="text-center">
+          로그인하면 모든 서비스를 이용할 수 있습니다.
+        </span>
+        <form onSubmit={handleSubmit(onValidSubmit)} className="flex flex-col">
+          <label>이메일</label>
+          <Input
+            register={{ ...register("email", emailValid()) }}
+            type="email"
+            placeholder="이메일을 입력해주세요."
+          />
+          <span>{errors.email?.message}</span>
+          <label>비밀번호</label>
+          <Input
+            register={{ ...register("password", passwordValid()) }}
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+          />
+          <span>{errors.password?.message}</span>
+          <button className="rounded-[30px] bg-slate-100 py-[18px]">
+            이메일로 로그인
+          </button>
+        </form>
+        <ul className="flex  justify-center space-x-[24px]">
+          <li className="flex flex-col items-center">
+            <button className="aspect-square w-[66px] rounded-full bg-[#E3E3E3]"></button>
+            <span>Google</span>
+          </li>
+          <li className="flex flex-col items-center">
+            <button
+              onClick={onClickKakao}
+              className="aspect-square w-[66px] rounded-full bg-[#E3E3E3]"
+            ></button>
+            <span>Kakao</span>
+          </li>
+          <li className="flex flex-col items-center">
+            <button className="aspect-square w-[66px] rounded-full bg-[#E3E3E3]"></button>
+            <span>Social</span>
+          </li>
+        </ul>
+        <span onClick={() => navigate("/signup")} className="cursor-pointer">
+          모인다 회원가입
+        </span>
+      </main>
+    </div>
   );
 };
 
