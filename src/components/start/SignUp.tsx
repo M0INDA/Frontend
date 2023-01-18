3400c05cf5daef9a60c9ecdd13d558ca66680be7
hansejun <tpwnszja2@gmail.com>
2023-01-19 02:17:10 +0900

    #16 [modify] 회원가입 스타일 수정

--- src/components/start/SignUp.tsx
+++ src/components/start/SignUp.tsx
@@ -1,4 +1,6 @@
+import ErrorMessage from "@elements/ErrorMessage";
 import Input from "@elements/Input";
+import cls from "@utils/cls";
 import { emailValid, nicknameValid, passwordValid } from "@utils/valids";
 import { ISignUp } from "allTypes/user";
 import React, { useCallback, useState } from "react";
@@ -8,6 +10,7 @@ const SignUp = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<ISignUp>();
 
@@ -37,52 +40,75 @@ const SignUp = () => {
   }, []);
 
   return (
-    <form
-      onSubmit={handleSubmit(onValidSubmit)}
-      className="mx-auto flex w-80 flex-col"
-    >
-      <div>
-        <Input
-          register={{ ...register("email", emailValid()) }}
-          type="email"
-          placeholder="이메일을 입력해주세요."
-          label="이메일"
-          buttonText="이메일 인증"
-          onClick={emailDup}
-        />
-        <button onClick={emailDup}>
-          {codeNum ? "인증번호 재전송" : "이메일 인증"}
-        </button>
-      </div>
-      {codeNum !== 0 && (
-        <div>
+    <>
+      <h1 className="mt-[2.4rem] text-[1.3rem] font-medium text-primary-700">
+        회원가입
+      </h1>
+      <form
+        onSubmit={handleSubmit(onValidSubmit)}
+        className="flex flex-col pt-[20px]"
+      >
+        <div className="mb-[1.6rem] flex flex-col space-y-[10px]">
           <Input
-            register={{ ...register("emailCode") }}
-            type="number"
-            placeholder="인증번호"
+            register={{ ...register("email", emailValid()) }}
+            type="email"
+            placeholder="이메일을 입력해주세요."
+            label="이메일"
+            buttonText="이메일 인증"
+            onClick={emailDup}
           />
-          <button onClick={codeValid}>인증</button>
-        </div>
-      )}
+          {errors?.email && <ErrorMessage text={errors.email?.message} />}
 
-      <span>{errors.email?.message}</span>
-      <Input
-        register={{ ...register("password", passwordValid()) }}
-        type="password"
-        placeholder="Password"
-      />
-      <span>{errors.password?.message}</span>
-      <div>
+          {codeNum !== 0 && (
+            <Input
+              register={{ ...register("emailCode", { required: true }) }}
+              type="number"
+              placeholder="인증번호"
+              buttonText="인증 확인"
+              onClick={codeValid}
+            />
+          )}
+        </div>
+
+        <div className="mb-[1.6rem]">
+          <Input
+            register={{ ...register("nickname", nicknameValid()) }}
+            type="text"
+            placeholder="닉네임을 입력해주세요."
+            buttonText="중복검사"
+            onClick={nicknameDup}
+            label="닉네임"
+          />
+          <ErrorMessage text={errors.nickname?.message} />
+        </div>
         <Input
-          register={{ ...register("nickname", nicknameValid()) }}
-          type="text"
-          placeholder="Nickname"
+          register={{ ...register("password", passwordValid()) }}
+          type="password"
+          placeholder="비밀번호는 영어, 특수문자 포함 8~20자리 입니다."
+          label="비밀번호"
         />
-        <button onClick={nicknameDup}>중복검사</button>
-      </div>
-      <span>{errors.nickname?.message}</span>
-      <button>회원가입</button>
-    </form>
+        <ErrorMessage text={errors.password?.message} className="mb-[10px]" />
+        <Input
+          register={{ ...register("passwordCheck", passwordValid()) }}
+          type="password"
+          placeholder="비밀번호를 다시 한번 입력해주세요."
+        />
+        <ErrorMessage
+          text={errors.passwordCheck?.message}
+          className="mb-[10px]"
+        />
+        <button
+          className={cls(
+            "mt-[10vh] rounded-[30px] bg-[rgba(0,0,0,0.05)] py-[18px] text-primary-400 transition-colors",
+            watch("password")?.length > 7 && watch("passwordCheck")?.length > 7
+              ? "bg-primary-main text-primary-100"
+              : ""
+          )}
+        >
+          가입하기
+        </button>
+      </form>
+    </>
   );
 };
 
