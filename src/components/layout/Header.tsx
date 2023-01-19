b23f969d3046fc1636701bbd9a02e3c0a4b7e6f4
hansejun <tpwnszja2@gmail.com>
2023-01-20 04:17:11 +0900

    [add] 스타일 예약어 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <header className="flex h-[6rem] w-full items-center justify-between bg-slate-600 ">
-      <nav className="flex">
-        <span>MODIDA</span>
+    <header className="flex h-[6rem] w-full items-center justify-between bg-slate-600 px-[13vw]">
+      <nav className="font flex">
+        <span className="H1">MODIDA</span>
         <ul className="flex">
           <li>홈</li>
           <li>마이페이지</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         primary: {
           main: "#ED7868",
+          sub1: "EE6648",
           100: "#FFFFFF",
           200: "#E1E1E1",
           300: "#E3E3E3",
