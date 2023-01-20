d39d05f4bf565e1784656fd434368dc28a60444c
doeun <drakequation@naver.com>
2023-01-20 18:01:57 +0900

    #19 [add] 카테고리 버튼 구현

--- src/components/main/CategoryBtn.tsx
+++ src/components/main/CategoryBtn.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import cls from "@utils/cls";
+
 const CategoryBtn = () => {
   const categorys = [
     "전체 분야",
@@ -7,12 +10,22 @@ const CategoryBtn = () => {
     "고시, 공무원",
     "기타",
   ];
+  const [click, setClick] = useState("전체 분야");
+  console.log(click);
   return (
     <div>
-      <div className="h-[5.4rem] w-[21rem] rounded-full bg-primary-500 px-9 py-4 text-white">
-        안녕
-      </div>
-      <div className="h-[5.4rem] w-[21rem] px-9 py-4">안녕</div>
+      {categorys.map((cate) => (
+        <div
+          key={cate}
+          onClick={() => setClick(cate)}
+          className={cls(
+            "H2 h-[5.4rem] w-[21rem]  cursor-pointer py-[1.4rem] px-[3.6rem]",
+            cate === click ? "rounded-full bg-primary-500  text-white" : ""
+          )}
+        >
+          {cate}
+        </div>
+      ))}
     </div>
   );
 };
