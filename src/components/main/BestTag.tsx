0de402f5906b1f586294a052ef26c61744731eca
doeun <drakequation@naver.com>
2023-01-22 08:20:58 +0900

    #33 [add] 마이페이지 기본 틀 구현

--- src/components/main/BestTag.tsx
+++ src/components/main/BestTag.tsx
@@ -1,9 +1,9 @@
 const BestTag = () => {
   return (
-    <div className="mt-[6.5rem]">
+    <div className="mt-[7.2rem]">
       <h2 className="H2 mb-[1.8rem] text-primary-600">오늘의 인기 키워드</h2>
       <div className="mt-[1.8rem]">
-        <div className="Sub1 w-fit rounded-[5rem] border border-solid border-primary-400 py-[0.8rem] px-[1.6rem] text-primary-600">
+        <div className="Sub2 w-fit rounded-[5rem] border border-solid border-primary-200 py-[0.8rem] px-[1.6rem] text-primary-600">
           인기태그
         </div>
       </div>
