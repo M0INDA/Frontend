4851914d6c01f062344e685d2c9717abdd62cdd0
hansejun <tpwnszja2@gmail.com>
2023-01-22 03:46:50 +0900

    #23 [add] 뽀모도로 구현 완료

--- src/components/main/Pomodoro.tsx
+++ src/components/main/Pomodoro.tsx
@@ -1,8 +1,12 @@
+import Timer from "@components/timer/Timer";
+
 const Pomodoro = () => {
   return (
-    <div className="mt-[7rem]">
+    <div className="mt-[7rem] flex flex-col">
       <h2 className="H2 mb-[1.8rem] text-primary-600">뽀모도로</h2>
-      <div className="h-[21.8rem] w-[44.6rem] rounded-[1rem] bg-primary-300"></div>
+      <div className="relative h-[21.8rem] w-[44.6rem] rounded-[1rem] bg-primary-sub3">
+        <Timer />
+      </div>
     </div>
   );
 };
