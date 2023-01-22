8fdf5dc865a1e7c0c7683aaf495fd58867aadc90
doeun <drakequation@naver.com>
2023-01-23 04:48:23 +0900

    #40 [modify] 스터디 아이콘들 추가

--- src/components/main/Studing.tsx
+++ src/components/main/Studing.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import Icon1 from "@assets/icons/Icon1.svg";
+import NextSvg from "@assets/svg/NextSvg.svg";
 
 const Studing = () => {
   return (
     <div className="mt-[5.4rem] ">
       <h2 className="H2 mb-[1.8rem] text-primary-600">참여 중인 스터디그룹</h2>
       <div className="flex h-[11.8rem] w-[44.6rem] items-center rounded-[1.4rem] border border-solid border-primary-350 px-[1.6rem]">
-        <div className="h-[5.4rem] w-[5.4rem] rounded-[1.2rem] bg-green-400" />
+        <div className="flex-center h-[5.4rem] w-[5.4rem] rounded-[1.2rem] bg-green-400">
+          <img src={Icon1} alt="studyIcon" className="h-[4rem] w-[4rem]" />
+        </div>
         <div className="ml-[1.5rem]">
           <p className="Sub2">토익 900 달성 스터디</p>
           <p className="Cap4 text-primary-500">#어학 #매일피드백 #700이상</p>
@@ -20,6 +23,11 @@ const Studing = () => {
             </div>
           </div>
         </div>
+        <img
+          src={NextSvg}
+          alt="NextIcon"
+          className="ml-auto h-[2.4rem] w-[2.4rem]"
+        />
       </div>
     </div>
   );
