1328247a59e89077ad785e0e19d2e81ee376a21c
doeun <drakequation@naver.com>
2023-01-25 00:16:09 +0900

    #33 [update] 프로필 아이콘 추가

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,4 +1,5 @@
 import SettingSvg from "@assets/svg/SettingSvg.svg";
+import SmileSvg from "@assets/svg/SmileSvg.svg";
 
 const Profile = () => {
   return (
@@ -7,6 +8,7 @@ const Profile = () => {
         <span className="H2">내 프로필</span>
         <img
           src={SettingSvg}
+          alt="settingcon"
           className="h-[2.2rem] w-[2.2rem] cursor-pointer"
         />
       </div>
@@ -19,14 +21,26 @@ const Profile = () => {
       </div>
       <div className="mt-[3.8rem] flex gap-[2.6rem]">
         <div className="flex h-[8rem] w-[18.6rem] rounded-[1rem] bg-primary-sub3 py-[1.5rem] px-[1.7rem]">
-          <div className="h-[4.6rem] w-[4.6rem] rounded-[1rem] bg-white"></div>
+          <div className="flex-center h-[4.6rem] w-[4.6rem] rounded-[1rem] bg-white">
+            <img
+              src={SmileSvg}
+              alt="smilecon"
+              className="h-[2.4rem] w-[2.4rem]"
+            />
+          </div>
           <div className="ml-[1.3rem]">
             <p className="Cap3 text-primary-500">평가점수</p>
             <p className="H2 text-primary-600">4점</p>
           </div>
         </div>
         <div className="flex h-[8rem] w-[18.6rem] rounded-[1rem] bg-primary-sub3 py-[1.5rem] pl-[1.7rem]">
-          <div className="h-[4.6rem] w-[4.6rem] rounded-[1rem] bg-white"></div>
+          <div className="flex-center h-[4.6rem] w-[4.6rem] rounded-[1rem] bg-white">
+            <img
+              src={SmileSvg}
+              alt="smilecon"
+              className="h-[2.4rem] w-[2.4rem]"
+            />
+          </div>
           <div className="ml-[1.3rem]">
             <p className="Cap3 text-primary-500">누적 공부시간</p>
             <p className="H2 text-primary-600">900h 59m</p>
