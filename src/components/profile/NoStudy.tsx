0e6c8647db00dff77eaccd6298aea0c20e10cb7c
doeun <drakequation@naver.com>
2023-01-30 20:10:06 +0900

    #33 [modify] 최근 공부 기록 css 수정

--- src/components/profile/NoStudy.tsx
+++ src/components/profile/NoStudy.tsx
@@ -2,7 +2,7 @@ import Icon16 from "@assets/icons/Icon16.svg";
 
 const NoStudy = () => {
   return (
-    <div className="flex-center h-[38.6rem] w-[50.4rem] flex-col rounded-[1rem] border border-dashed border-primary-200">
+    <div className="flex-center h-[100vh] max-h-[38.6rem] w-[100vw] max-w-[50.4rem] flex-col rounded-[1rem] border border-dashed border-primary-200">
       <img src={Icon16} alt="NoStudy" className="h-[3.4rem] w-[4rem]" />
       <p className="Cap1 pt-[1rem] text-primary-600">
         아래 ‘스터디 그룹 가입하기' 버튼을 눌러 <br />
