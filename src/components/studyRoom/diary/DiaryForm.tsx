3ef115d99c1bbdc7e5bb18a30bd0ac9d0b577823
hansejun <tpwnszja2@gmail.com>
2023-02-02 16:59:46 +0900

    #51 [add] Text editor 추가

--- src/components/studyRoom/diary/DiaryForm.tsx
+++ src/components/studyRoom/diary/DiaryForm.tsx
@@ -1,5 +1,52 @@
+import { useMemo, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 const DiaryForm = () => {
-  return <div className="mx-[3rem] rounded-xl border p-[3rem]">DiaryForm</div>;
+  const [value, setValue] = useState("");
+  const handleOnClick = () => console.log("hello");
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ size: ["small", false, "large", "huge"] }, { color: [] }],
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+            { align: [] },
+          ],
+          ["image", "video"],
+        ],
+      },
+    }),
+    []
+  );
+
+  return (
+    <div className="relative flex flex-col justify-end  bg-bgColor-100 px-[3rem] ">
+      <ReactQuill
+        modules={modules}
+        theme="snow"
+        value={value}
+        onChange={setValue}
+      />
+      <div className="z-10 flex items-center justify-between px-[1rem]">
+        <div>
+          <button>아이콘</button>
+        </div>
+        <button
+          onClick={handleOnClick}
+          className="Cap3  cursor-pointer rounded-[1rem] bg-primary-main p-[0.7rem_1.5rem] text-primary-100"
+        >
+          보내기
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DiaryForm;
--- src/components/studyRoom/diary/GroupDiary.tsx
+++ src/components/studyRoom/diary/GroupDiary.tsx
@@ -5,7 +5,7 @@ const GroupDiary = () => {
   return (
     <div className="flex h-full min-w-[59.3rem] flex-col space-y-[2.6rem]">
       <DiarySearch />
-      <div className="flex h-full flex-col rounded-[1rem] bg-bgColor-100 pb-[3rem]">
+      <div className="flex h-full flex-col rounded-[1rem] bg-bgColor-100 pb-[3rem] ">
         <DiaryList />
         <DiaryForm />
       </div>
