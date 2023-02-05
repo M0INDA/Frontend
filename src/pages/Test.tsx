ca7004aa00a79abbe502fd56e8bc369c6c145181
hansejun <tpwnszja2@gmail.com>
2023-02-05 21:39:23 +0900

    [test] test

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from "react";
-
 const Test = () => {
-  const [count, setCount] = useState(0);
-  const [isLoading,setIsLoading] = useState
+  // const [count, setCount] = useState(0);
+  // const [isLoading,setIsLoading] = useState
 
-  useEffect(()=>{
-    
-  },[])
+  // useEffect(()=>{
 
-  console.log("");
+  // },[])
+
+  // console.log("");
   return <div>Test</div>;
 };
 
